f5bfaaa8025213c6f076d6507eed1db8f6f793ad
박철현 <tghy4@naver.com>
2024-08-20 21:07:13 +0900

    Transactional Propagation 예졔

---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    @Test
+    void outerTxOff_success() {
+
+    }
+
+}
